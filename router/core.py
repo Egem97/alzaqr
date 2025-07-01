867750e88576c453b8f31915471339afbfc1ddfb
egem97 <pierenriquez5@gmail.com>
2025-07-01 14:50:03 -0500

    PHL add

--- router/core.py
+++ router/core.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from views.tools import qrtool, dashboard, qrgenerator
+from views.prod import explorer_prod_excel
 
 
 
@@ -8,10 +9,11 @@ from views.tools import qrtool, dashboard, qrgenerator
 def pages():
     page_dict = {}
     page_dict["Qr Tool"] = [
-        st.Page(page = qrtool,title="Qr Tool",icon = ":material/home:"),
-        st.Page(page = qrgenerator,title="Qr Generator",icon = ":material/home:"),    
+        st.Page(page = qrtool,title="Qr Tool",icon = "📇"),
+        st.Page(page = qrgenerator,title="Qr Generator",icon = "🛠️"),   
+        st.Page(page = dashboard,title="Datos Agrupados",icon = "🔎") 
     ]
-    page_dict["Dashboard"] = [
-        st.Page(page = dashboard,title="Dashboard",icon = ":material/home:")
+    page_dict["Producción"] = [
+        st.Page(page = explorer_prod_excel,title="Explorer Data",icon = "📈")
     ]
     return page_dict
--- utils/components.py
+++ utils/components.py
@@ -44,3 +44,46 @@ def aggrid_builder(dataframe = pd.DataFrame(),columns_totales = []):
         #width = "100%",
         #theme = "light"
     )
+
+
+
+def aggrid_builder_prod(dataframe = pd.DataFrame(),columns_totales = []):
+    gb = GridOptionsBuilder.from_dataframe(dataframe)
+    #gb.configure_pagination(enabled=True)
+    #gb.configure_side_bar()
+    #gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+    #gb.configure_column("action", cellRenderer="agGroupCellRenderer", headerName="Action", width=100)
+    cellstyle_last_row = JsCode(f"""
+                function(params) {{
+                    if (params.node.rowIndex === {len(dataframe) - 1}) {{
+                        return {{
+                            'font-weight': 'bold',
+                            'background': '#33373a',
+                            'color': '#fff',
+                            'font-size': '16px'
+                        }}
+                    }}
+                }}
+    """)
+    # Aplica el estilo a todas las columnas
+    for col in dataframe.columns:
+        gb.configure_column(col, cellStyle=cellstyle_last_row)
+    grid_options = gb.build()
+
+    row_height = 35
+    header_height = 35
+    num_rows = len(dataframe)
+    min_height = 120
+    max_height = 600
+    height = min(max(header_height + (num_rows * row_height), min_height), max_height)
+
+    return AgGrid(
+        dataframe,
+        gridOptions = grid_options,
+        update_mode = 'MODEL_CHANGED',#GridUpdateMode.NO_UPDATE,
+        height = height,
+        fit_columns_on_grid_load=True,
+        allow_unsafe_jscode=True
+        #width = "100%",
+        #theme = "light"
+    )
